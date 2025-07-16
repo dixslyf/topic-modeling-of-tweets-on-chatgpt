bd01aab0e0c1b516bb174b88d20f1c6410a6945e
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-07-16 22:28:09 +0800

    feat(flake): compile report with typix

--- flake.nix
+++ flake.nix
@@ -2,16 +2,77 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    typix = {
+      url = "github:loqusion/typix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
+  outputs =
     {
-      devShell = pkgs.mkShell {
-        packages = with pkgs; [
-          typst
-        ];
-      };
-    });
+      nixpkgs,
+      flake-utils,
+      typix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) lib;
+
+        typixLib = typix.lib.${system};
+
+        reportBuildArgs = {
+          typstSource = "report.typ";
+          src = lib.fileset.toSource {
+            root = ./report;
+            fileset = lib.fileset.unions [
+              (lib.fileset.fromSource (typixLib.cleanTypstSource ./report))
+              ./report/graphics
+              ./report/references.bib
+            ];
+          };
+        };
+
+        report = typixLib.buildTypstProject reportBuildArgs;
+        build-report = typixLib.buildTypstProjectLocal reportBuildArgs;
+        watch-report = typixLib.watchTypstProject { typstSource = "report/report.typ"; };
+      in
+      {
+        devShell = typixLib.devShell {
+          packages = [
+            build-report
+            watch-report
+          ];
+        };
+
+        checks = {
+          inherit
+            report
+            build-report
+            watch-report
+            ;
+        };
+
+        packages = {
+          inherit report;
+          default = report;
+        };
+
+        apps =
+          let
+            build-report-app = flake-utils.lib.mkApp {
+              drv = build-report;
+            };
+          in
+          {
+            build-report = build-report-app;
+            default = build-report-app;
+
+            watch-report = flake-utils.lib.mkApp {
+              drv = watch-report;
+            };
+          };
+      }
+    );
 }
